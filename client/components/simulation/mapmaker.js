45ec53b5af6288036d210ab4c6ecd46a3c1a1e01
jjyc <johnr0hol@gmail.com>
2018-11-30 00:12:48 -0500

    taking off map

--- client/components/simulation/mapmaker.js
+++ client/components/simulation/mapmaker.js
@@ -15,7 +15,6 @@ class MapMaker extends Component{
     }
 
     componentDidMount(){
-        Meteor.call('maps.updatewall', [0,1])
         this.setState({'totalwidth': parseInt(d3.select('.svg-container').style('width'))})
         this.setState({'totalheight': parseInt(d3.select('.svg-container').style('height'))})
         var _this = this
